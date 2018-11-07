7ed9e109a4527a1170ace2ca7ebd240f4c787fb5
sbx_user1060 <sbx_user1060@ip-10-129-147-156.eu-west-2.compute.internal>
2018-11-07 23:00:04 +0000

    Wed Nov 07 23:00:04 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,6 +6,7 @@ def checkout(skus):
 
     items = {}
     price = {'A': 50, 'B':30, 'C':20, 'D': 15, 'E':40, 'F':10, 'G':20, 'H':10, 'I':35, 'J':60, 'K':80, 'L':90, 'M':15, 'N':40, 'O':10, 'P': 50, 'Q':30, 'R':50, 'S':30, 'T':20, 'U':40, 'V':50, 'W':20, 'X':90, 'Y':10, 'Z':50}
+    special = {'A':0, 'B':0, 'E':0, 'F':0, 'H':0, 'K':0, 'N':0, 'P':0, 'Q':0, 'R':0, 'U':0, 'V':0}
 
     for char in 'ABCDEFGHIJKLMNOPQRSTVXYZ':
         items[char] = 0
@@ -17,33 +18,30 @@ def checkout(skus):
 
         items[char] += 1
     
-    specialA = 0
     while items['A'] >= 3:
         if items['A'] >= 5:
-            specialA += 200
+            special['A'] += 200
             items['A'] -= 5
         else:
-            specialA += 130
+            special['A'] += 130
             items['A'] -= 3
 
-
-    specialE = 0
     while items['E'] >= 2 and items['B'] > 0:
-        specialE += 80
+        special['E'] += 80
         items['E'] -= 2
         items['B'] -= 1
 
-    specialB = 0
     while items['B'] >= 2:
-        specialB += 45
+        special['B'] += 45
         items['B'] -= 2
 
-    specialF = 0
     while items['F'] >= 3:
-        specialF += 20
+        special['F'] += 20
         items['F'] -= 3
 
-    special_sum = specialF + specialA + specialB + specialE
+    special_sum = 0
+    for char in special:
+        special_sum += special[char]
 
     usual_sum = 0
     for char in items:
