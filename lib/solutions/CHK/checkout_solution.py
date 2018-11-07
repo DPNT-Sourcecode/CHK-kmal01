358a90ccd055785cbe67ef349c43f23d0727e419
sbx_user1060 <sbx_user1060@ip-10-129-147-156.eu-west-2.compute.internal>
2018-11-07 22:31:35 +0000

    Wed Nov 07 22:31:35 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,8 +3,13 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+
     items = {'A': 0, 'B': 0, 'C': 0, 'D':0}
     for char in skus:
+
+        if char != 'A' and char != 'B' and char != 'C' and char != 'D':
+            return -1
+
         items[char] += 1
     
     specialA = 0
@@ -18,6 +23,6 @@ def checkout(skus):
         items['B'] -= 2
 
 
-    total_sum = specialA + items['A']*50 + items['B'] * 30 + items['C'] * 20 + items['D'] * 15 
+    total_sum = specialA + specialB + items['A']*50 + items['B'] * 30 + items['C'] * 20 + items['D'] * 15 
 
     return total_sum
