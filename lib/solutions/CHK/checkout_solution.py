e11247d51360c34973bfab31846606a10c168ebc
sbx_user1060 <sbx_user1060@ip-10-129-147-156.eu-west-2.compute.internal>
2018-11-07 22:42:33 +0000

    Wed Nov 07 22:42:33 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,18 +4,29 @@
 # skus = unicode string
 def checkout(skus):
 
-    items = {'A': 0, 'B': 0, 'C': 0, 'D':0}
+    items = {'A': 0, 'B': 0, 'C': 0, 'D':0, 'E': 0}
     for char in skus:
 
-        if char != 'A' and char != 'B' and char != 'C' and char != 'D':
+        if char != 'A' and char != 'B' and char != 'C' and char != 'D' and char != 'E':
             return -1
 
         items[char] += 1
     
     specialA = 0
     while items['A'] >= 3:
-        specialA += 130
-        items['A'] -= 3
+        if items['A'] >= 5:
+            specialA += 200
+            items['A'] -= 5
+        else:
+            specialA += 130
+            items['A'] -= 3
+
+
+    specialE = 0
+    while items['E'] >= 2:
+        specialE += 40
+        items['E'] -= 2
+        items['B'] -= 1
 
     specialB = 0
     while items['B'] >= 2:
@@ -23,6 +34,6 @@ def checkout(skus):
         items['B'] -= 2
 
 
-    total_sum = specialA + specialB + items['A']*50 + items['B'] * 30 + items['C'] * 20 + items['D'] * 15 
+    total_sum = specialA + specialB + specialE + items['A']*50 + items['B'] * 30 + items['C'] * 20 + items['D'] * 15 + items['E'] * 40
 
     return total_sum
