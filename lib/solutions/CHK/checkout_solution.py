3050213c5cf28c61c9530ab9ec00e721d68952ac
sbx_user1060 <sbx_user1060@ip-10-129-147-156.eu-west-2.compute.internal>
2018-11-07 22:25:36 +0000

    Wed Nov 07 22:25:36 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,7 +3,7 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    items = {'A': 0, 'B': 0, 'C': 0, 'D':}
+    items = {'A': 0, 'B': 0, 'C': 0, 'D':0}
     for char in skus:
         items[char] += 1
     
